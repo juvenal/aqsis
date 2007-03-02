0bbd18ff60368b6962a026e55e29c5c1e55d682d
Chris Foster <c42f@users.sourceforge.net>
2007-03-02 15:11:10 +0000

    Improved and tested the CqRunningState class - should now be somewhat usable.
    git-svn-id: https://aqsis.svn.sourceforge.net/svnroot/aqsis/branches/runningstate_refactor@908 30f9f022-c70d-0410-a6f5-8c645ca7840b

--- shadercompiler/shadervm/runningstate.cpp
+++ shadercompiler/shadervm/runningstate.cpp
@@ -1,44 +1,60 @@
-#include "runningstate.h"
 #include <algorithm>
+#include <iterator>
+#include <assert.h>
+
+#include "runningstate.h"
 
 namespace Aqsis {
 
-//----------------------------------------------------------------------
-void CqRunningState::Intersect(const CqRunningState& from)
+//------------------------------------------------------------------------------
+CqRunningState::CqRunningState(CqBitVector& bitVector)
+	: m_data(),
+	m_maxLength(0)
 {
-	CqRunningState::RsVector oldData = m_data;
-	CqRunningState::RsVector::iterator dataEnd = std::set_intersection(oldData.begin(), oldData.end(),
-			from.m_data.begin(), from.m_data.end(), m_data.begin());
-	m_data.erase(dataEnd, m_data.end());
+	fromBitVector(bitVector);
 }
 
-
-//----------------------------------------------------------------------
-void CqRunningState::Complement()
+//------------------------------------------------------------------------------
+void CqRunningState::intersect(const CqRunningState& from)
 {
 	CqRunningState::RsVector oldData = m_data;
-	m_data.resize(m_length - m_data.size());
-	CqRunningState::RsVector::iterator newI = m_data.begin();
+	m_data.clear();
+	std::set_intersection(oldData.begin(), oldData.end(),
+			from.m_data.begin(), from.m_data.end(), std::back_inserter(m_data));
+}
+
+//------------------------------------------------------------------------------
+void CqRunningState::complement()
+{
+	CqRunningState::RsVector oldData = m_data;
+	m_data.clear();
 	CqRunningState::RsVector::iterator oldI = oldData.begin();
-	for(TqInt j = 0; j < m_length; j++)
+	for(TqUint j = 0; j < m_maxLength; j++)
 	{
 		if(oldI != oldData.end() && j == *oldI)
 			oldI++;
 		else
-		{
-			*newI = j;
-			newI++;
-		}
+			m_data.push_back(j);
 	}
 }
 
+//------------------------------------------------------------------------------
+void CqRunningState::unionRS(const CqRunningState& from)
+{
+	CqRunningState::RsVector oldData = m_data;
+	m_data.clear();
+	std::set_union(oldData.begin(), oldData.end(),
+			from.m_data.begin(), from.m_data.end(), std::back_inserter(m_data));
+}
 
-//----------------------------------------------------------------------
+//------------------------------------------------------------------------------
 void CqRunningState::fromBitVector(CqBitVector& bv)
 {
+	m_maxLength = bv.Size();
+	m_data.reserve(m_maxLength);
 	m_data.resize(bv.Count());
 	CqRunningState::RsVector::iterator i = m_data.begin();
-	for(TqInt j = 0; j < bv.Size(); j++)
+	for(TqUint j = 0; j < m_maxLength; j++)
 	{
 		if(bv.Value(j))
 		{
@@ -48,6 +64,14 @@ void CqRunningState::fromBitVector(CqBitVector& bv)
 	}
 }
 
-//----------------------------------------------------------------------
+//------------------------------------------------------------------------------
+std::ostream& operator<<( std::ostream &stream, CqRunningState& rsOut)
+{
+	for(CqRunningState::RsVector::iterator itr = rsOut.m_data.begin();
+			itr != rsOut.m_data.end(); itr++)
+		stream << *itr << " ";
+	return stream;
+}
+//------------------------------------------------------------------------------
 
 } // namespace Aqsis
--- shadercompiler/shadervm/runningstate.h
+++ shadercompiler/shadervm/runningstate.h
@@ -2,54 +2,79 @@
 #define RUNNINGSTATE_H_INCLUDED
 
 #include <vector>
+#include <iostream>
 
 #include "aqsis.h"
+#include "bitvector.h"
 
 namespace Aqsis {
 
-//-----------------------------------------------------------------------
+//------------------------------------------------------------------------------
 class  CqRunningState
 {
 	public:
-		typedef TqInt value_type;
-		typedef std::vector<value_type> RsVector;
-		typedef RsVector::size_type size_type;
-		typedef RsVector::iterator iterator;
+		typedef TqUint value_type;
+		typedef std::vector<value_type>::size_type size_type;
+		typedef std::vector<value_type>::const_iterator iterator;
 
-		CqRunningState(TqInt length = 0)
-			: m_data(),
-			m_length(length)
-		{
-			m_data.reserve(length);
-		}
-		void Intersect(const CqRunningState& from);
-		void Complement();
+		CqRunningState(TqInt maxLength = 0);
+		// Use default copy constructor.
+		CqRunningState(CqBitVector& bitVector);
+		void intersect(const CqRunningState& from);
+		void complement();
+		void unionRS(const CqRunningState& from);
 		void fromBitVector(CqBitVector& bv);
-		iterator begin()
-		{
-			return m_data.begin();
-		}
-		iterator end()
-		{
-			return m_data.end();
-		}
-		CqRunningState& operator=(const CqRunningState& from)
-		{
-			m_data = from.m_data;
-			m_length = from.m_length;
-			return *this;
-		}
-		value_type operator[](const size_type idx)
-		{
-			return m_data[idx];
-		}
+		iterator begin() const;
+		iterator end() const;
+		size_type size() const;
+		size_type maxSize() const;
+		// Use default assignment operator
+		value_type operator[](const size_type idx) const;
+
+		friend std::ostream& operator<<(std::ostream &stream, CqRunningState& rsOut);
 	private:
+		typedef std::vector<value_type> RsVector;
 		RsVector m_data;
-		TqInt	m_length;
+		size_type m_maxLength;
 };
 
-//-----------------------------------------------------------------------
 
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Implementation for CqRunningState inline functions.
+inline CqRunningState::CqRunningState(TqInt maxLength)
+	: m_data(),
+	m_maxLength(maxLength)
+{
+	m_data.reserve(maxLength);
+}
+
+inline CqRunningState::iterator CqRunningState::begin() const
+{
+	return m_data.begin();
+}
+
+inline CqRunningState::iterator CqRunningState::end() const
+{
+	return m_data.end();
+}
+
+inline CqRunningState::size_type CqRunningState::size() const
+{
+	return m_data.size();
+}
+
+inline CqRunningState::size_type CqRunningState::maxSize() const
+{
+	return m_maxLength;
+}
+
+inline CqRunningState::value_type CqRunningState::operator[](const size_type idx) const
+{
+	return m_data[idx];
+}
+
+//------------------------------------------------------------------------------
 } // namespace Aqsis
 
 #endif // !RUNNINGSTATE_H_INCLUDED
