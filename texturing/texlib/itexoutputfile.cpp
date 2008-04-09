juvenal/aqsis

commit b2523f0e97e35ff1639ccb93bf4cdadb458dc697

* Remove tiled output interfaces - they will be subsumed into a single texture   output interface which will do tiled output depending on a flag. * Remove the redundent Attr::IsTiled flag in favour of testing whether the   TileInfo structure is present. * Remove TileWidth and TileHeight texture attributes in favour of a single   TileInfo structure attribute. * Add an erase() method to CqTexFileHeader for erasing unneeded attributes. * Other minor cleanups
git-svn-id: https://aqsis.svn.sourceforge.net/svnroot/aqsis/trunk/aqsis@2036 30f9f022-c70d-0410-a6f5-8c645ca7840b

texturing/texlib/itexoutputfile.cpp
@@ -32,6 +32,7 @@
 namespace Aqsis {
 
 //------------------------------------------------------------------------------
+// IqTexOutputFile implementation
 boost::shared_ptr<IqTexOutputFile> IqTexOutputFile::open(
 		const std::string& fileName, EqImageFileType fileType,
 		const CqTexFileHeader& header)
@@ -64,6 +65,17 @@ boost::shared_ptr<IqTexOutputFile> IqTexOutputFile::open(
 	}
 
 	return newFile;
+}
+
+
+//------------------------------------------------------------------------------
+// IqMultiTexOutputFile implementation
+boost::shared_ptr<IqMultiTexOutputFile> IqMultiTexOutputFile::open(
+		const std::string& fileName, EqImageFileType fileType,
+		const CqTexFileHeader& header)
+{
+	/// \todo Implementation
+	return boost::shared_ptr<IqMultiTexOutputFile>();
 }
 
 } // namespace Aqsis

texturing/texlib/itexoutputfile.h
@@ -46,6 +46,11 @@ namespace Aqsis {
  * uniform interface for placing image data into a file.  Metadata is handled
  * via the CqTexFileHeader container, while pixel data may be written into the
  * file one line at a time, or a whole set of lines at once.
+ *
+ * Although the interface is scanline-oriented, output of tiled data is
+ * possible for those formats which support it.  The user signals the intent
+ * to produce tiled output data by setting the appropriate attributes in the
+ * file attribute header.
  */
 class AQSISTEX_SHARE IqTexOutputFile
 {
@@ -119,6 +124,31 @@ class AQSISTEX_SHARE IqTexOutputFile
 		virtual void writePixelsImpl(const CqMixedImageBuffer& buffer) = 0;
 };
 
+
+//------------------------------------------------------------------------------
+/** \brief Texture output file interface supporting multiple sub-images
+ *
+ * Some file types, such as TIFF support storage of multiple sub-images inside
+ * a single file.  This interface supports such usage by allowing 
+ */
+class AQSISTEX_SHARE IqMultiTexOutputFile : public IqTexOutputFile
+{
+	public:
+		/** \brief Create a new subimage, finalizing the current one.
+		 */
+		virtual void newSubImage(TqInt width, TqInt height) = 0;
+
+		/** \brief Open an input image file in a given format
+		 *
+		 * \param fileName - file to open.  Can be in any of the formats
+		 * understood by aqsistex.
+		 * \param fileType - the file type.
+		 * \return The newly opened input file
+		 */
+		static boost::shared_ptr<IqMultiTexOutputFile> open(
+				const std::string& fileName, EqImageFileType fileType,
+				const CqTexFileHeader& header);
+};
 
 //==============================================================================
 // Implementation of inline functions and templates

texturing/texlib/tiffdirhandle.cpp
@@ -84,10 +84,11 @@ const char* tiffCompressionNameFromTag(uint16 compressionType)
 // CqTiffDirHandle
 //------------------------------------------------------------------------------
 
-CqTiffDirHandle::CqTiffDirHandle(const boost::shared_ptr<CqTiffFileHandle>& fileHandle, const tdir_t dirIdx)
+CqTiffDirHandle::CqTiffDirHandle(const boost::shared_ptr<CqTiffFileHandle>& fileHandle, const TqInt dirIdx)
 	: m_fileHandle(fileHandle)
 {
-	fileHandle->setDirectory(dirIdx);
+	if(dirIdx > 0)
+		fileHandle->setDirectory(dirIdx);
 }
 
 tdir_t CqTiffDirHandle::dirIndex() const
@@ -277,11 +278,11 @@ void CqTiffDirHandle::fillHeaderRequiredAttrs(CqTexFileHeader& header) const
 	// pixel memory layout.
 	header.set<Attr::Width>(tiffTagValue<uint32>(TIFFTAG_IMAGEWIDTH));
 	header.set<Attr::Height>(tiffTagValue<uint32>(TIFFTAG_IMAGELENGTH));
-	header.set<Attr::IsTiled>(TIFFIsTiled(tiffPtr()));
-	if(header.find<Attr::IsTiled>())
+	if(TIFFIsTiled(tiffPtr()))
 	{
-		header.set<Attr::TileWidth>(tiffTagValue<uint32>(TIFFTAG_TILEWIDTH));
-		header.set<Attr::TileHeight>(tiffTagValue<uint32>(TIFFTAG_TILELENGTH));
+		header.set<Attr::TileInfo>( SqTileInfo(
+					tiffTagValue<uint32>(TIFFTAG_TILEWIDTH),
+					tiffTagValue<uint32>(TIFFTAG_TILELENGTH)) );
 	}
 	// Get the compression type.
 	header.set<Attr::Compression>(

texturing/texlib/tiffdirhandle.h
@@ -80,10 +80,11 @@ class AQSISTEX_SHARE CqTiffDirHandle : boost::noncopyable
 		/** \brief Construct a tiff directory handle from a tiff file handle
 		 *
 		 * \param fileHandle - handle to the underlying tiff file
-		 * \param dirIdx - directory index
+		 * \param dirIdx - directory index.  If negative, use the current
+		 *                 subimage directory.
 		 */
 		CqTiffDirHandle(const boost::shared_ptr<CqTiffFileHandle>& fileHandle,
-				const tdir_t dirIdx = 0);
+				const TqInt dirIdx = 0);
 
 		/// Obtain the underlying tiff file pointer
 		TIFF* tiffPtr() const;

texturing/texlib/tiffoutputfile.cpp
@@ -57,8 +57,8 @@ void CqTiffOutputFile::initialize()
 	// make all channels are the same type.
 	if(channelList.sharedChannelType() == Channel_TypeUnknown)
 		AQSIS_THROW(XqInternal, "tiff cannot store multiple pixel types in the same image");
-	// reorder the channels for TIFF output...
-	channelList.reorderChannels();
+
+	// Maybe we should check here whether channels need reordering by name...
 
 	// Use lzw compression if the compression hasn't been specified.
 	std::string& compressionStr = m_header.find<Attr::Compression>();

texturing/texlib/CMakeLists.txt
@@ -19,7 +19,6 @@ SET(AQSISTEX_SRCS
 	ewafilter.cpp
 	cachedfilter.cpp
 	ishadowsampler.cpp
-	itiledtexoutputfile.cpp
 	itexinputfile.cpp
 	itexoutputfile.cpp
 	itexturesampler.cpp
@@ -50,7 +49,6 @@ SET(AQSISTEX_HDRS
 	imagechannel.h
 	imagefiletype.h
 	ishadowsampler.h
-	itiledtexoutputfile.h
 	itexinputfile.h
 	itexoutputfile.h
 	itexturemap_old.h

texturing/texlib/exrinputfile.cpp
@@ -126,8 +126,8 @@ void convertHeader(const Imf::Header& exrHeader, CqTexFileHeader& header)
 				displayBox.min.x - dataBox.min.x,
 				displayBox.min.y - dataBox.min.y) );
 
-	// tiling information
-	header.set<Attr::IsTiled>(exrHeader.hasTileDescription());
+	// Set tiling information ?
+
 	// Aspect ratio
 	header.set<Attr::PixelAspectRatio>(exrHeader.pixelAspectRatio());
 

texturing/texlib/itiledtexoutputfile.cpp
@@ -1,68 +1,0 @@
-// Aqsis
-// Copyright (C) 1997 - 2007, Paul C. Gregory
-//
-// Contact: pgregory@aqsis.org
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public
-// License as published by the Free Software Foundation; either
-// version 2 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-//
-// You should have received a copy of the GNU General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-/** \file
- *
- * \brief Implementation of a scanline-based output interface for texture files.
- *
- * \author Chris Foster
- */
-
-#include "itiledtexoutputfile.h"
-
-//#include "tiledtiffoutputfile.h"
-
-namespace Aqsis {
-
-//------------------------------------------------------------------------------
-boost::shared_ptr<IqTiledTexOutputFile>
-IqTiledTexOutputFile::open(const std::string& fileName,
-		EqImageFileType fileType, const CqTexFileHeader& header)
-{
-	// Check some of the header data to make sure it's minimally sane...
-	if(header.width() <= 0 || header.height() <= 0)
-	{
-		AQSIS_THROW(XqInternal, "Cannot open \"" << fileName
-				<< "\": Image width and height cannot be negative or zero.");
-	}
-	if(header.channelList().numChannels() == 0)
-	{
-		AQSIS_THROW(XqInternal, "Cannot open \"" << fileName
-				<< "\": No data channels present.");
-	}
-
-	// Create the new file object
-	boost::shared_ptr<IqTiledTexOutputFile> newFile;
-
-	switch(fileType)
-	{
-//		case ImageFile_Tiff:
-//			newFile.reset(new CqTiledTiffOutputFile(fileName, header));
-//			break;
-		// case ...:  // Add new output formats here!
-		default:
-			AQSIS_THROW(XqInternal, "Cannot open \"" << fileName
-					<< "\": Unknown file type \""
-					<< imageFileTypeToString(fileType) << "\"");
-	}
-
-	return newFile;
-}
-
-} // namespace Aqsis

commit 156b1fad35adfca71c358432cfa7cd4daba01657

* Implement support for tiled texture output and output of multiple subimages   in the CqTiffOutputFile class.   - CqTiffOutputFile now implements the IqMultiTexOutputFile interface for     multiple subimage support   - The desire for tiled output is indicated by setting the appropriate image     header attributes. * Fix a *major* regression introduced by r2036 - the smallest mipmap level was   being loaded instead of level 0 (!)
git-svn-id: https://aqsis.svn.sourceforge.net/svnroot/aqsis/trunk/aqsis@2037 30f9f022-c70d-0410-a6f5-8c645ca7840b

texturing/texlib/itexoutputfile.cpp
@@ -32,6 +32,32 @@
 namespace Aqsis {
 
 //------------------------------------------------------------------------------
+namespace {
+
+// Helper functions.
+
+/** Open a file with the IqMultiTexOutputFile interface
+ *
+ * \return an open file, or NULL if the file type is invalid.
+ */
+boost::shared_ptr<IqMultiTexOutputFile> openMultiOutputFile(
+		const std::string& fileName, EqImageFileType fileType,
+		const CqTexFileHeader& header)
+{
+	switch(fileType)
+	{
+		case ImageFile_Tiff:
+			return boost::shared_ptr<IqMultiTexOutputFile>(
+					new CqTiffOutputFile(fileName, header));
+		// case ...:  // Add new output formats here!
+		default:
+			return boost::shared_ptr<IqMultiTexOutputFile>();
+	}
+}
+
+}  // unnamed namespace
+
+//------------------------------------------------------------------------------
 // IqTexOutputFile implementation
 boost::shared_ptr<IqTexOutputFile> IqTexOutputFile::open(
 		const std::string& fileName, EqImageFileType fileType,
@@ -41,26 +67,32 @@ boost::shared_ptr<IqTexOutputFile> IqTexOutputFile::open(
 	if(header.width() <= 0 || header.height() <= 0)
 	{
 		AQSIS_THROW(XqInternal, "Cannot open \"" << fileName
-				<< "\": Image width and height cannot be negative or zero.");
+				<< "\" - image width and height cannot be negative or zero.");
 	}
 	if(header.channelList().numChannels() == 0)
 	{
 		AQSIS_THROW(XqInternal, "Cannot open \"" << fileName
-				<< "\": No data channels present.");
+				<< "\" - no data channels present.");
 	}
 
 	// Create the new file object
-	boost::shared_ptr<IqTexOutputFile> newFile;
+	boost::shared_ptr<IqTexOutputFile> newFile
+		= openMultiOutputFile(fileName, fileType, header);
+	if(newFile)
+		return newFile;
 
 	switch(fileType)
 	{
-		case ImageFile_Tiff:
-			newFile.reset(new CqTiffOutputFile(fileName, header));
-			break;
 		// case ...:  // Add new output formats here!
+		case ImageFile_Exr:
+		case ImageFile_Jpg:
+		case ImageFile_Png:
+			AQSIS_THROW(XqInternal, "Cannot open \"" << fileName
+					<< "\" - unimplemented file type \""
+					<< imageFileTypeToString(fileType) << "\"");
 		default:
 			AQSIS_THROW(XqInternal, "Cannot open \"" << fileName
-					<< "\": Unknown file type \""
+					<< "\" - unknown file type \""
 					<< imageFileTypeToString(fileType) << "\"");
 	}
 
@@ -74,8 +106,18 @@ boost::shared_ptr<IqMultiTexOutputFile> IqMultiTexOutputFile::open(
 		const std::string& fileName, EqImageFileType fileType,
 		const CqTexFileHeader& header)
 {
-	/// \todo Implementation
-	return boost::shared_ptr<IqMultiTexOutputFile>();
+	boost::shared_ptr<IqMultiTexOutputFile> newFile
+		= openMultiOutputFile(fileName, fileType, header);
+	if(newFile)
+	{
+		return newFile;
+	}
+	else
+	{
+		AQSIS_THROW(XqInternal, "Cannot open \"" << fileName
+				<< "\" - file type \"" << imageFileTypeToString(fileType) << "\""
+				<< " doesn't support multiple subimages");
+	}
 }
 
 } // namespace Aqsis

texturing/texlib/itexoutputfile.h
@@ -163,10 +163,17 @@ void IqTexOutputFile::writePixels(const Array2DType& buffer)
 		AQSIS_THROW(XqInternal, "Cannot put pixels from buffer into file \""
 				<< fileName() << "\": buffer has incorrect width.");
 	}
-	CqMixedImageBuffer newBuf(buffer.channelList(),
-			boost::shared_array<TqUint8>(const_cast<TqUint8*>(buffer.rawData()),
-				nullDeleter), buffer.width(), numScanlines);
-	writePixelsImpl(newBuf);
+	if(numScanlines > 0)
+	{
+		CqMixedImageBuffer newBuf(buffer.channelList(),
+				boost::shared_array<TqUint8>(const_cast<TqUint8*>(buffer.rawData()),
+					nullDeleter), buffer.width(), numScanlines);
+		writePixelsImpl(newBuf);
+	}
+	else
+	{
+		AQSIS_THROW(XqInternal, "Attempt to write buffer off the end of an image");
+	}
 }
 
 } // namespace Aqsis

texturing/texlib/mipmaplevelcache.h
@@ -234,12 +234,14 @@ const TextureBufferT& CqMipmapLevelCache<TextureBufferT>::level(TqInt levelNum)
 	assert(levelNum >= 0);
 	if(!m_levels[levelNum])
 	{
-		Aqsis::log() << debug << "loaded subtexture " << levelNum
-			<< " from texture " << m_texFile->fileName() << "\n";
 		// read in requested level if it's not loaded yet.
 		m_levels[levelNum].reset(new TextureBufferT());
 		m_texFile->setImageIndex(levelNum);
 		m_texFile->readPixels(*m_levels[levelNum]);
+		Aqsis::log() << debug << "loaded subtexture " << levelNum
+			<< " [" << m_levels[levelNum]->width() << "x"
+			<< m_levels[levelNum]->width() << "] " 
+			<< "from texture " << m_texFile->fileName() << "\n";
 	}
 	return *m_levels[levelNum];
 }

texturing/texlib/tiffdirhandle.cpp
@@ -84,11 +84,10 @@ const char* tiffCompressionNameFromTag(uint16 compressionType)
 // CqTiffDirHandle
 //------------------------------------------------------------------------------
 
-CqTiffDirHandle::CqTiffDirHandle(const boost::shared_ptr<CqTiffFileHandle>& fileHandle, const TqInt dirIdx)
+CqTiffDirHandle::CqTiffDirHandle(const boost::shared_ptr<CqTiffFileHandle>& fileHandle, const tdir_t dirIdx)
 	: m_fileHandle(fileHandle)
 {
-	if(dirIdx > 0)
-		fileHandle->setDirectory(dirIdx);
+	fileHandle->setDirectory(dirIdx);
 }
 
 tdir_t CqTiffDirHandle::dirIndex() const
@@ -250,6 +249,7 @@ void CqTiffDirHandle::writeOptionalAttrs(const CqTexFileHeader& header)
 	addAttributeToTiff<Attr::HostName,const char*>(TIFFTAG_HOSTCOMPUTER, header, *this);
 	addAttributeToTiff<Attr::Description,const char*>(TIFFTAG_IMAGEDESCRIPTION, header, *this);
 	addAttributeToTiff<Attr::DateTime,const char*>(TIFFTAG_DATETIME, header, *this);
+	addAttributeToTiff<Attr::TextureFormat,const char*>(TIFFTAG_PIXAR_TEXTUREFORMAT, header, *this);
 
 	// Add some matrix attributes
 	addAttributeToTiff<Attr::WorldToScreenMatrix,const float*>(
@@ -258,10 +258,28 @@ void CqTiffDirHandle::writeOptionalAttrs(const CqTexFileHeader& header)
 			TIFFTAG_PIXAR_MATRIX_WORLDTOCAMERA, header, *this);
 
 	/** \todo Add the following optional attributes:
-	 *  - "textureFormat" TIFFTAG_PIXAR_TEXTUREFORMAT,
-	 *  - "textureWrapMode" TIFFTAG_PIXAR_WRAPMODES,
-	 *  - "fieldOfViewCotan" TIFFTAG_PIXAR_FOVCOT,
+	 *  - "FieldOfViewCotan" TIFFTAG_PIXAR_FOVCOT,
 	 */
+
+	// Set texture wrap mode string
+	const SqWrapModes* wrapModes = header.findPtr<Attr::WrapModes>();
+	if(wrapModes)
+	{
+		std::ostringstream oss;
+		oss << wrapModeToString(wrapModes->sWrap)
+			<< " " << wrapModeToString(wrapModes->tWrap);
+		setTiffTagValue<const char*>(TIFFTAG_PIXAR_WRAPMODES, oss.str().c_str());
+	}
+
+	// Set tile dimensions if present.
+	const SqTileInfo* tileInfo = header.findPtr<Attr::TileInfo>();
+	if(tileInfo)
+	{
+		setTiffTagValue<uint32>(TIFFTAG_TILEWIDTH, tileInfo->width);
+		setTiffTagValue<uint32>(TIFFTAG_TILELENGTH, tileInfo->height);
+	}
+
+	// Set size of display window if present
 	const SqImageRegion* displayWindow = header.findPtr<Attr::DisplayWindow>();
 	if(displayWindow)
 	{
@@ -572,6 +590,15 @@ CqTiffFileHandle::CqTiffFileHandle(std::ostream& outputStream)
 	{
 		AQSIS_THROW(XqInternal, "Could not use output stream for tiff");
 	}
+}
+
+
+void CqTiffFileHandle::writeDirectory()
+{
+	assert(!m_isInputFile);
+	if(!TIFFWriteDirectory(m_tiffPtr.get()))
+		AQSIS_THROW(XqInternal, "Could not write tiff subimage to file");
+	++m_currDir;
 }
 
 void CqTiffFileHandle::setDirectory(tdir_t dirIdx)

texturing/texlib/tiffdirhandle.h
@@ -80,11 +80,11 @@ class AQSISTEX_SHARE CqTiffDirHandle : boost::noncopyable
 		/** \brief Construct a tiff directory handle from a tiff file handle
 		 *
 		 * \param fileHandle - handle to the underlying tiff file
-		 * \param dirIdx - directory index.  If negative, use the current
-		 *                 subimage directory.
+		 * \param dirIdx - directory index which this handle will point to.
+		 *                 Only relevant if the underlying file is open for input.
 		 */
 		CqTiffDirHandle(const boost::shared_ptr<CqTiffFileHandle>& fileHandle,
-				const TqInt dirIdx = 0);
+				const tdir_t = 0);
 
 		/// Obtain the underlying tiff file pointer
 		TIFF* tiffPtr() const;
@@ -253,6 +253,14 @@ class AQSISTEX_SHARE CqTiffFileHandle : boost::noncopyable
 
 		/// Return the file name
 		inline const std::string& fileName() const;
+
+		/** \brief Write the current directoy to file, and increment the
+		 * directory index.
+		 *
+		 * The tiff file must be open for output - calling this function for an
+		 * input file is an error.
+		 */
+		void writeDirectory();
 
 	private:
 		friend class CqTiffDirHandle;

texturing/texlib/tiffoutputfile.cpp
@@ -26,6 +26,11 @@
 
 #include "tiffoutputfile.h"
 
+#include <cstring>  // for memcpy.
+
+#include <boost/scoped_array.hpp>
+
+#include "aqsismath.h"
 #include "tiffdirhandle.h"
 
 namespace Aqsis {
@@ -51,14 +56,26 @@ const char* CqTiffOutputFile::fileName() const
 	return m_fileHandle->fileName().c_str();
 }
 
+EqImageFileType CqTiffOutputFile::fileType()
+{
+	return ImageFile_Tiff;
+}
+
+const CqTexFileHeader& CqTiffOutputFile::header() const
+{
+	return m_header;
+}
+
+TqInt CqTiffOutputFile::currentLine() const
+{
+	return m_currentLine;
+}
+
 void CqTiffOutputFile::initialize()
 {
-	CqChannelList& channelList = m_header.channelList();
-	// make all channels are the same type.
-	if(channelList.sharedChannelType() == Channel_TypeUnknown)
+	// make sure all channels are the same type.
+	if(m_header.channelList().sharedChannelType() == Channel_TypeUnknown)
 		AQSIS_THROW(XqInternal, "tiff cannot store multiple pixel types in the same image");
-
-	// Maybe we should check here whether channels need reordering by name...
 
 	// Use lzw compression if the compression hasn't been specified.
 	std::string& compressionStr = m_header.find<Attr::Compression>();
@@ -75,10 +92,21 @@ void CqTiffOutputFile::initialize()
 	dirHandle.writeHeader(m_header);
 }
 
-void CqTiffOutputFile::writePixelsImpl(const CqMixedImageBuffer& buffer)
+void CqTiffOutputFile::newSubImage(TqInt width, TqInt height)
 {
-	if(!buffer.channelList().channelTypesMatch(header().channelList()))
-		AQSIS_THROW(XqInternal, "Buffer and file channels don't match");
+	m_header.set<Attr::Width>(width);
+	m_header.set<Attr::Height>(height);
+
+	m_fileHandle->writeDirectory();
+
+	// Write header data to this directory.
+	/// \todo The header may be trimmed for directories after the first.
+	CqTiffDirHandle dirHandle(m_fileHandle);
+	dirHandle.writeHeader(m_header);
+}
+
+void CqTiffOutputFile::writeScanlinePixels(const CqMixedImageBuffer& buffer)
+{
 	CqTiffDirHandle dirHandle(m_fileHandle);
 	// Simplest possible implementation using scanline TIFF I/O.  Could use
 	// Strip-based IO if performance is ever a problem here.
@@ -87,11 +115,89 @@ void CqTiffOutputFile::writePixelsImpl(const CqMixedImageBuffer& buffer)
 	const TqInt endLine = m_currentLine + buffer.height();
 	for(TqInt line = m_currentLine; line < endLine; ++line)
 	{
-		TIFFWriteScanline(dirHandle.tiffPtr(), reinterpret_cast<tdata_t>(const_cast<TqUint8*>(rawBuf)),
-				static_cast<uint32>(line));
+		TIFFWriteScanline( dirHandle.tiffPtr(),
+				reinterpret_cast<tdata_t>(const_cast<TqUint8*>(rawBuf)),
+				static_cast<uint32>(line) );
 		rawBuf += rowStride;
 	}
 	m_currentLine = endLine;
+}
+
+
+namespace {
+
+/** Strided memory copy.
+ *
+ * Copies numElems data elements from src to dest.  Each data element (eg,
+ * contiguous group of pixels) has size given by elemSize bytes.  The stride
+ * between one data element and the next is given in bytes.
+ */
+void stridedCopy(TqUint8* dest, TqInt destStride, const TqUint8* src, TqInt srcStride,
+		TqInt numElems, TqInt elemSize)
+{
+	for(TqInt i = 0; i < numElems; ++i)
+	{
+		memcpy(dest, src, elemSize);
+		dest += destStride;
+		src += srcStride;
+	}
+}
+
+} // unnamed namespace
+
+
+void CqTiffOutputFile::writeTiledPixels(const CqMixedImageBuffer& buffer)
+{
+	SqTileInfo tileInfo = m_header.find<Attr::TileInfo>();
+	// Check that the buffer has a height that is a multiple of the tile height.
+	if( buffer.height() % tileInfo.height != 0
+		&& m_currentLine + buffer.height() != m_header.height() )
+	{
+		AQSIS_THROW(XqInternal, "pixel buffer with height = " << buffer.height()
+				<< " must be a multiple of requested tile height (= " << tileInfo.height
+				<< ") or run exactly to the full image height (= " << m_header.height()
+				<< ").");
+	}
+
+	CqTiffDirHandle dirHandle(m_fileHandle);
+	const TqUint8* rawBuf = buffer.rawData();
+	const TqInt bytesPerPixel = buffer.channelList().bytesPerPixel();
+	boost::scoped_array<TqUint8> tileBuf(
+			new TqUint8[bytesPerPixel*tileInfo.width*tileInfo.height]);
+	const TqInt rowStride = bytesPerPixel*buffer.width();
+	const TqInt endLine = m_currentLine + buffer.height();
+	const TqInt tileCols = buffer.width()/tileInfo.width;
+	for(TqInt line = m_currentLine; line < endLine; line += tileInfo.height)
+	{
+		// srcBuf will point to the beginning of the memory region which will
+		// become the tile.
+		const TqUint8* srcBuf = rawBuf;
+		for(TqInt tileCol = 0; tileCol < tileCols; ++tileCol)
+		{
+			const TqInt tileRowStride = min(bytesPerPixel*tileInfo.width,
+					rowStride - tileCol*bytesPerPixel*tileInfo.width);
+			// Copy parts of the scanlines into the tile buffer.
+			stridedCopy(tileBuf.get(), tileRowStride, srcBuf, rowStride,
+					tileInfo.height, tileRowStride);
+
+			TIFFWriteTile(dirHandle.tiffPtr(),
+					reinterpret_cast<tdata_t>(const_cast<TqUint8*>(tileBuf.get())),
+					tileCol*tileInfo.width, line, 0, 0);
+			srcBuf += tileRowStride;
+		}
+		rawBuf += rowStride*tileInfo.height;
+	}
+	m_currentLine = endLine;
+}
+
+void CqTiffOutputFile::writePixelsImpl(const CqMixedImageBuffer& buffer)
+{
+	if(!buffer.channelList().channelTypesMatch(m_header.channelList()))
+		AQSIS_THROW(XqInternal, "Buffer and file channels don't match");
+	if(m_header.findPtr<Attr::TileInfo>())
+		writeTiledPixels(buffer);
+	else
+		writeScanlinePixels(buffer);
 }
 
 } // namespace Aqsis

texturing/texlib/tiffoutputfile.h
@@ -38,7 +38,7 @@ class CqTiffFileHandle;
 //------------------------------------------------------------------------------
 /** \brief Scanline-based output for the TIFF file format.
  */
-class AQSISTEX_SHARE CqTiffOutputFile : public IqTexOutputFile
+class AQSISTEX_SHARE CqTiffOutputFile : public IqMultiTexOutputFile
 {
 	public:
 		/** \brief Construct a tiff output file with the given file name.
@@ -58,7 +58,13 @@ class AQSISTEX_SHARE CqTiffOutputFile : public IqTexOutputFile
 		virtual EqImageFileType fileType();
 		virtual const CqTexFileHeader& header() const;
 		virtual TqInt currentLine() const;
+		virtual void newSubImage(TqInt width, TqInt height);
+
 	private:
+		/// Write pixel buffer to file as scanlines
+		void writeScanlinePixels(const CqMixedImageBuffer& buffer);
+		/// Write pixel buffer to file in tiled format
+		void writeTiledPixels(const CqMixedImageBuffer& buffer);
 		// inherited
 		virtual void writePixelsImpl(const CqMixedImageBuffer& buffer);
 
@@ -73,25 +79,6 @@ class AQSISTEX_SHARE CqTiffOutputFile : public IqTexOutputFile
 		boost::shared_ptr<CqTiffFileHandle> m_fileHandle;
 };
 
-
-//==============================================================================
-// Implementation details
-//==============================================================================
-
-inline EqImageFileType CqTiffOutputFile::fileType()
-{
-	return ImageFile_Tiff;
-}
-
-inline const CqTexFileHeader& CqTiffOutputFile::header() const
-{
-	return m_header;
-}
-
-inline TqInt CqTiffOutputFile::currentLine() const
-{
-	return m_currentLine;
-}
 
 } // namespace Aqsis
 

texturing/texlib/wrapmode.h
@@ -51,6 +51,8 @@ enum EqWrapMode
 EqWrapMode wrapModeFromString(const std::string& modeString);
 
 
+/// Return a string representing the given wrap mode.
+const char* wrapModeToString(EqWrapMode mode);
 
 //==============================================================================
 // Implementation details
@@ -68,6 +70,24 @@ inline EqWrapMode wrapModeFromString(const std::string& modeString)
 		// Default: return black... maybe we should have an "unknown" wrap mode
 		// to signal errors...
 		return WrapMode_Black;
+}
+
+inline const char* wrapModeToString(EqWrapMode mode)
+{
+	switch(mode)
+	{
+		case WrapMode_Black:
+			return "black";
+		case WrapMode_Periodic:
+			return "periodic";
+		case WrapMode_Clamp:
+			return "clamp";
+		case WrapMode_Trunc:
+			return "trunc";
+		default:
+			assert(0);
+			return "unknown";
+	}
 }
 
 } // namespace Aqsis